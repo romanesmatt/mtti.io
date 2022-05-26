165c635f373aaf5f876ff54e3c4440dfe824c08e
romanesmatt <mattromanes@gmail.com>
2022-05-26 16:16:04 +1200

    Massive changes to UI

--- src/App.js
+++ src/App.js
@@ -1,38 +1,73 @@
 import './App.css';
 import { Box, Typography, Paper, Container } from "@mui/material";
+import BackgroundImage from "./assets/images/queenstown.jpeg";
 
 function App() {
   return (
-    <Box
-      component="span"
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        backgroundColor: "#000080",
+    <div
+      style={{
+        backgroundImage: `url(${BackgroundImage})`,
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+        backgroundPosition: "center",
+        backgroundSize: "cover"
       }}
-    >
 
+    >
       <Box
+        component="span"
         sx={{
           display: "flex",
           justifyContent: "center",
-          marginTop: "10%",
-          flexDirection: "column",
-          alignItems: "center"
         }}
       >
-        <Typography sx={{ fontWeight: "bold", fontSize: 40, color: "white" }}>
-          {/* Matt Romanes */}
-        </Typography>
-        <Box>
-          <Typography sx={{ fontSize: 20, color: "white" }}>
-            Hello! I am Matt Romanes. More to come.
-          </Typography>
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "8%",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <Paper sx={{
+            minWidth: "40%",
+            minHeight: "100%",
+            opacity: "70%",
+            backgroundColor: "black"
+          }}>
+            <Typography sx={{ fontWeight: "bold", fontSize: 50, color: "white" }}>
+              Hello! I'm Matt Romanes.
+            </Typography>
+            <Box>
+              <p>
+                <Typography sx={{ fontSize: 20, color: "white" }}>
+                 More to come.
+                </Typography>
+              </p>
+
+              <p>
+                <Typography sx={{ fontSize: 20, color: "white" }}>
+                </Typography>
+              </p>
+
+              <p>
+                <Typography sx={{ fontSize: 20, color: "white" }}>
+                  
+                </Typography>
+              </p>
+
+            </Box>
+
+          </Paper>
+
         </Box>
 
       </Box>
 
-    </Box>
+    </div>
+
 
   );
 }
