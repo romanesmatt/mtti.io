2c7ca329d4717f971f45d78fe20c1137b5f80c94
Matt Romanes <48772682+romanesmatt@users.noreply.github.com>
2022-06-01 17:42:51 +1200

    Clean up code and change background

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Box, Typography, Paper, Container, BottomNavigation, BottomNavigationAc
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
-import BackgroundImage from "./queenstown.JPG";
+import BackgroundImage from "./rotoiti.JPG";
 
 export default function App() {
     // State handling for the Bottom Navigation component
@@ -13,14 +13,14 @@ export default function App() {
     // Responsive design
 
     return (
-        // Background image container
+        // Background image container (also the main container)
         <Box
             sx={{
                 backgroundImage: `url(${BackgroundImage})`,
                 backgroundRepeat: 'no-repeat',
                 height: '100vh',
-                minHeight: "100%",
-                backgroundPosition: "center",
+                minHeight: "90vh",
+                backgroundPosition: "bottom",
                 backgroundSize: "cover",
                 backgroundRepeat: "no-repeat",
                 backgroundAttachment: "scroll",
@@ -32,61 +32,50 @@ export default function App() {
             }}
 
         >
-            {/* Main container of page */}
-            <Box
-                component="span"
-                sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    height: "80%",
-                    
-                }}
-            >
-
                 {/* Container for title and text */}
 
                 <Box
                     sx={{
                         display: "flex",
-                        justifyContent: "center",
                         flexDirection: "column",
                         alignItems: "center",
                         backgroundColor: "rgba(0, 0, 0, 0.8)",
                         height: "auto",
-                        width: "50%",
-                        borderRadius: "16px",
+                        width: "85vh",
+                        borderRadius: "10px",
                         boxShadow: 1,
-                        overflow: "auto"
+                        overflowY: "auto",
                     }}
                 >
-                    <Typography sx={{ fontWeight: "bold", fontSize: 60, color: "white", marginTop: "2%" }}>
+                    <h1>
+                    <Typography sx={{ fontWeight: "bold", fontSize: 60, color: "white", }}>
                         Hello! I'm Matt Romanes.
                     </Typography>
-                    <Box sx={{ width: "67%", marginBottom: "5%", marginTop: "2%", }}>
+                    </h1>
+
+                    <Box sx={{ width: "67%", marginBottom: "2%"  }}>
                         <p>
-                            <Typography sx={{ fontSize: 20, color: "white" }}>
-                                I am a Computer Science student, Software Developer and technology enthusiast from Wellington, New Zealand.
+                            <Typography sx={{ fontSize: 18, color: "white" }}>
+                                I'm a Computer Science student, Software Developer and technology enthusiast from Wellington, New Zealand.
                             </Typography>
                         </p>
 
                         <p>
-                            <Typography sx={{ fontSize: 20, color: "white" }}>
-                                I'm a passionate, team-oriented and hard-working individual who wants to create better digital solutions.
+                            <Typography sx={{ fontSize: 18, color: "white" }}>
+                                I describe myself as a passionate, team-oriented and hard-working individual who wants to create better digital solutions.
                                 As a result, I'm seeking roles in Software Development and Cybersecurity that will help me achieve this goal.
                             </Typography>
                         </p>
 
                         <p>
-                            <Typography sx={{ fontSize: 20, color: "white" }}>
-                                My most prominent languages are JavaScript, Java and Python. I've worked with full-stack, DevOps,
+                            <Typography sx={{ fontSize: 18, color: "white" }}>
+                                My most prominent languages are JavaScript, Java and Python. Additionally, I've worked with full-stack, DevOps,
                                 cybersecurity and networking technology, both in my academics and employment.
                             </Typography>
                         </p>
 
                         <p>
-                            <Typography sx={{ fontSize: 20, color: "white" }}>
+                            <Typography sx={{ fontSize: 18, color: "white" }}>
                                 I'm currently in my final semester at Victoria University of
                                 Wellington and a part-time Software Developer at CodeCamp.
                             </Typography>
@@ -98,27 +87,22 @@ export default function App() {
                     <BottomNavigation
                         showLabels display
                         value={value}
-                        onChange={(event, newValue) => {
-                            setValue(newValue);
-                        }}
                         sx={{
                             backgroundColor: "rgba(0, 0, 0, 0)",
                             marginBottom: "5%",
-                            width: "100%"
+                            width: "40%"
                         }}
                     >
-                        <BottomNavigationAction icon={<a href="https://github.com/romanesmatt"> <GitHubIcon sx={{ color: "white", fontSize: "50px" }} /></a>} />
-                        <BottomNavigationAction icon={<a href="https://www.linkedin.com/in/matt-romanes-860606153/"><LinkedInIcon sx={{ color: "white", fontSize: "50px" }} /></a>} />
-                        <BottomNavigationAction icon={<a href="mailto:mattromanes@gmail.com"><EmailIcon sx={{ color: "white", fontSize: "50px" }} /></a>} />
+                        <BottomNavigationAction icon={<a href="https://github.com/romanesmatt"> <GitHubIcon sx={{ color: "white", fontSize: "40px" }} /></a>} />
+                        <BottomNavigationAction icon={<a href="https://www.linkedin.com/in/matt-romanes-860606153/"><LinkedInIcon sx={{ color: "white", fontSize: "40px" }} /></a>} />
+                        <BottomNavigationAction icon={<a href="mailto:mattromanes@gmail.com"><EmailIcon sx={{ color: "white", fontSize: "40px" }} /></a>} />
                     </BottomNavigation>
 
-                    <Typography sx={{ fontSize: 12, color: "white", marginBottom: "2%" }}>
+                    <Typography sx={{ fontSize: 11, color: "white", marginBottom: "2%" }}>
                         © Matt Romanes
                     </Typography>
                 </Box>
 
             </Box>
-
-        </Box>
     );
 }
